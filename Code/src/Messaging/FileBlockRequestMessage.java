7225706946f5fd2c65bfb7d05a5d95c0ed197b68
Reis <80530563+JoaoReis153@users.noreply.github.com>
2026-04-23 08:29:57 +0100

    feat: allow for a dynamic sizing of blocks

--- Code/src/Messaging/FileBlockRequestMessage.java
+++ Code/src/Messaging/FileBlockRequestMessage.java
@@ -8,6 +8,9 @@ import java.util.List;
 public class FileBlockRequestMessage implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    private static final int DEFAULT_TARGET_BLOCK_COUNT = 64;
+    private static final int MIN_BLOCK_SIZE = 1024;
+    private static final int MAX_BLOCK_SIZE = 1024 * 1024;
 
     private byte[] hash;
     private long offset;
@@ -88,12 +91,26 @@ public class FileBlockRequestMessage implements Serializable {
         );
     }
  
-    // A method that has already the blockSize defined
     public static List<FileBlockRequestMessage> createBlockList(
         byte[] hash,
         long fileSize
     ) {
-        return createBlockList(hash, fileSize, 10240);
+        return createBlockList(hash, fileSize, getDynamicBlockSize(fileSize));
+    }
+
+    private static int getDynamicBlockSize(long fileSize) {
+        if (fileSize <= 0) {
+            return MIN_BLOCK_SIZE;
+        }
+
+        long proposedBlockSize = (fileSize + DEFAULT_TARGET_BLOCK_COUNT - 1) /
+            DEFAULT_TARGET_BLOCK_COUNT;
+        long boundedBlockSize = Math.max(
+            MIN_BLOCK_SIZE,
+            Math.min(MAX_BLOCK_SIZE, proposedBlockSize)
+        );
+
+        return (int) boundedBlockSize;
     }
 
     /*
